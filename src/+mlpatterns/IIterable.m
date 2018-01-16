6ebc9d86519f4ecc162aacac150964bf20d143a2
John J. Lee <16329959+jjleewustledu@users.noreply.github.com>
2018-01-15 18:31:14 -0600

    Prior to refactoring mlrois.BrainMaskBuilder.

---
+++ src/+mlpatterns/IIterable.m
@@ -0,0 +1,15 @@
+classdef (Abstract) IIterable 
+	%% IITERABLE  
+
+	%  $Revision$
+ 	%  was created 14-Jan-2018 20:02:16 by jjlee,
+ 	%  last modified $LastChangedDate$ and placed into repository /Users/jjlee/Local/src/mlcvl/mlpatterns/src/+mlpatterns.
+ 	%% It was developed on Matlab 9.3.0.713579 (R2017b) for MACI64.  Copyright 2018 John Joowon Lee.
+ 	
+	properties (Abstract) 		
+        iterator
+    end
+
+	%  Created with Newcl by John J. Lee after newfcn by Frank Gonzalez-Morphy
+ end
+
